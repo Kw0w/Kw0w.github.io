1cfb0c8bd3f854f38ce0f2b449593848bab3957e
Alex Visigoth <xk1335@gmail.com>
2022-09-26 14:46:21 +0200

    Add Analog Clock

---
+++ underPortfolio/Clock/script.js
@@ -0,0 +1,26 @@
+//Set on site start
+setInterval(setClock, 1000);
+
+//Variables
+const hourHand = document.querySelector("[data-hour-hand]");
+const minuteHand = document.querySelector("[data-minute-hand]");
+const secondHand = document.querySelector("[data-second-hand]");
+
+//Set clock
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+}
+
+//Set Arrow Rotation
+function setRotation(element, rotationRation) {
+    element.style.setProperty("--rotation", rotationRation * 360);
+}
+
+setClock();
+
